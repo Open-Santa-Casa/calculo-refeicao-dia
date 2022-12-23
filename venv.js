61752d89086e8ae4a78c5746f07b6c0aadde5faa
Flávio E Pimentel <63177834+flavioepimentel@users.noreply.github.com>
2022-12-23 12:13:10 -0300

    ✨ feat: Cria substituto do '.env'

---
+++ venv.js
@@ -0,0 +1,10 @@
+/**
+ *  🔐 Esse arquivo serve como um substituto do .env, que não é suportado no AppScripts 
+ * e tem a finalidade de armazenar as variáveis de ambiente como constantes. Basta substituir
+ * o valor atribuido as constantes e remover as barras '//' de comentário, 
+ * o AppScripts lida e resolve as importações entre os arquivos.
+ */
+
+const folder_id_cardapio // = 'ID DA PASTA DE CARDÁPIOS'
+const planilha_id_bdNutricao // = 'ID DA PLANILHA DO SETOR NUTRIÇÃO'
+const folder_id_compras // = 'ID DA PASTA DE ESTOQUE'
